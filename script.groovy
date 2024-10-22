7a7040e6a0a617be561d47bad739eb3d480f0ed8
MG <105832307+MohamedGouda99@users.noreply.github.com>
2024-10-22 18:46:26 +0300

    ..

--- script.groovy
+++ script.groovy
@@ -120,6 +120,7 @@ def authWithAWS() {
 def getKubeConfig(clusterName, region) {
     echo "Retrieving kubeconfig for EKS cluster ${env.clusterName} in ${env.region}..."
     sh "aws eks update-kubeconfig --name ${env.clusterName} --region ${env.region}"
+    sh "Got kubeconfig"
 }
 
 return this
